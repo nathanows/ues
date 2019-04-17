4a0440bfd4c65f234ae93bb1f92f8aa2248933cb
Nathan Owsiany <nowsiany@gmail.com>
2019-04-16 20:42:28 -0600

    Builds in concurrency to client

--- client/main.go
+++ client/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/nathanows/ues/echo"
@@ -23,12 +25,27 @@ func main() {
 
 	c := echo.NewEchoServiceClient(conn)
 
-	req := echo.EchoRequest{Message: "something"}
-	log.Printf("Request: %#v", req)
-	response, err := c.Echo(context.Background(), &req)
+	ch := make(chan string)
+	for _, msg := range os.Args[1:] {
+		go makeRequest(msg, c, ch)
+	}
+
+	for range os.Args[1:] {
+		log.Printf(<-ch)
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Completed %d requests in %s", len(os.Args[1:]), elapsed)
+}
+
+func makeRequest(msg string, client echo.EchoServiceClient, ch chan<- string) {
+	start := time.Now()
+	req := echo.EchoRequest{Message: msg}
+	log.Printf("Request: %s", req.Message)
+	resp, err := client.Echo(context.Background(), &req)
 	if err != nil {
 		log.Fatalf("error when calling Echo: %s", err)
 	}
 	elapsed := time.Since(start)
-	log.Printf("Response: %#v, Took: %s", response, elapsed)
+	ch <- fmt.Sprintf("Response: %+v, Took: %s", resp.Message, elapsed)
 }
